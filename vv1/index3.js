f13dae7494db33de04f419be357b07f872b68531
RVX <rvx.movie@gmail.com>
2026-01-17 07:55:42 +0530

    Fix import casing for showLoader and hideLoader

--- vv1/index3.js
+++ vv1/index3.js
@@ -5,7 +5,7 @@
 import {loadPlayerDeps}  from "https://cdn.jsdelivr.net/gh/rvx-apps/NeoPlayer@main/vv1/utils/loadlibs.js";
 import MenuController from "https://cdn.jsdelivr.net/gh/rvx-apps/NeoPlayer@main/vv1/utils/menu.js";
 import {isFullscreen} from "https://cdn.jsdelivr.net/gh/rvx-apps/NeoPlayer@main/vv1/utils/fullscreenhelp.js";
-import {showloader, hideLoader} from "https://cdn.jsdelivr.net/gh/rvx-apps/NeoPlayer@main/vv1/utils/loader.js";
+import {showLoader, hideLoader} from "https://cdn.jsdelivr.net/gh/rvx-apps/NeoPlayer@main/vv1/utils/loader.js";
 import {parseSRT,parseVTT,parseSub} from "https://cdn.jsdelivr.net/gh/rvx-apps/NeoPlayer@main/vv1/utils/subtitles.js";
 import {timeStringToMs, toSeconds} from "https://cdn.jsdelivr.net/gh/rvx-apps/NeoPlayer@main/vv1/utils/timeformat.js";
 
